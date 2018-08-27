268cf53274cb1abfe4a4f5bba9d6b35a1000e819
Benedikt Naessens <benedikt.naessens@robovision.eu>
2018-08-27 20:53:06 +0200

    Some debug info

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -43,6 +43,7 @@ class TLDetector(object):
 
         config_string = rospy.get_param("/traffic_light_config")
         self.config = yaml.load(config_string)
+        print(self.config)
 
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
 
