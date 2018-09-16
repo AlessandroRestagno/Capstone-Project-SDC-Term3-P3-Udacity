abd88daf157989413e4462a273f780ba9e87fcc9
FeLIX Mueller <git@felixm.net>
2018-09-16 13:17:52 +0000

    Classification: make paths relative

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -28,7 +28,10 @@ class TLClassifier(object):
         self.model_final = Model(inputs = base_model.input, outputs = predictions)
 
         sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
-        self.model_final.load_weights("/home/workspace/Capstone-Project-SDC-Term3-P3-Udacity/ros/src/tl_detector/light_classification/resnet50_5.h5")
+        # self.model_final.load_weights("/home/workspace/Capstone-Project-SDC-Term3-P3-Udacity/ros/src/tl_detector/light_classification/resnet50_5.h5")
+        cwd = os.path.dirname(os.path.realpath(__file__))
+        rospy.loginfo('TLC cwd %s', cwd)
+        self.model_final.load_weights(cwd + '/resnet50_5.h5')
 
         self.graph = tf.get_default_graph()
         
@@ -36,7 +39,7 @@ class TLClassifier(object):
         self.labels = ['green', 'no', 'orange', 'red']
 
         self.model_loaded = 1
-        rospy.loginfo("Classification model loaded.")
+        rospy.loginfo("TLC Classification model loaded.")
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
