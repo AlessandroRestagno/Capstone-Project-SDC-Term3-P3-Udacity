abb86bfcfdf866fe82bca7e59db30b0a6ce6975f
Alessandro Restagno <resta89@gmail.com>
2018-10-02 13:27:37 +0200

    Target velocity correction
    I introduced target velocity variable to consider the target velocity of the Parking lot. So, every time we change target velocity, we don't have to change the code.

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -12,14 +12,15 @@ class Controller(object):
     def __init__(self, vehicle_mass, fuel_capacity, brake_deadband, decel_limit, accel_limit, wheel_radius, wheel_base, steer_ratio, max_lat_accel, max_steer_angle, max_throttle_percent):
         self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
 
-        kp = 0.5
-        ki = 0.0001
-        kd = 0.15
-        mn = decel_limit #minial throttle value
-        mx = max_throttle_percent #maximum throttle value
-        self.throttle_controller = PID(kp, ki, kd, mn, mx)
+        #We didn't use a throttle PID controller
+        #kp = 0.5
+        #ki = 0.0001
+        #kd = 0.15
+        #mn = decel_limit #minial throttle value
+        #mx = max_throttle_percent #maximum throttle value
+        #self.throttle_controller = PID(kp, ki, kd, mn, mx)
 
-        tau = 0.05 # 1 / (2pi*tau) = cutoff frequency
+        tau = 0.05  #1 / (2pi*tau) = cutoff frequency
         ts = 0.02 # sample time
         self.vel_lpf = LowPassFilter(tau, ts)
 
@@ -30,43 +31,46 @@ class Controller(object):
         self.accel_limit = accel_limit
         self.wheel_radius = wheel_radius
 
-        self.last_time = rospy.get_time()
+        #self.last_time = rospy.get_time()
+        self.max_vel = 0
         self.last_throttle = 0
         self.last_brake = 100
 
     def control(self, linear_vel, angular_vel, current_vel, dbw_enabled, max_throttle_percent):
         # return throttle, brake, steer
+
         if not dbw_enabled:
-            self.throttle_controller.reset()
             return 0., 0., 0.
+        
+        target_vel = linear_vel
+        if target_vel > self.max_vel:
+            self.max_vel = target_vel
         
         filt_current_vel = self.vel_lpf.filt(current_vel)
         #rospy.loginfo('angular_vel: %f', angular_vel)
         #steering = self.yaw_controller.get_steering(twist.twist.linear.x, twist.twist.angular.z, velocity.twist.linear.x)
-
         steering = self.yaw_controller.get_steering(linear_vel, angular_vel, filt_current_vel)
 
         vel_error = linear_vel - filt_current_vel
         
-        #rospy.loginfo('linear_vel: %f', linear_vel)
-        #rospy.loginfo('filt_current_vel: %f', filt_current_vel)
-        #rospy.loginfo('vel_error: %f', vel_error)
-        current_time = rospy.get_time()
-        sample_time = current_time - self.last_time
-        self.last_time = current_time
+        #current_time = rospy.get_time()
+        #sample_time = current_time - self.last_time
+        #self.last_time = current_time
 
-        acceleration = self.throttle_controller.step(vel_error, sample_time)
+        #acceleration = self.throttle_controller.step(vel_error, sample_time)
         
-        # smooth acceleration: http://ijssst.info/Vol-17/No-30/paper19.pdf
-        smooth_acc = ((linear_vel * linear_vel) - (filt_current_vel * filt_current_vel)) / (2 * 30)
+        # smooth acceleration algorithm based on "An Intelligent Vehicle Based on an Improved PID Speed Control Algorithm for Driving Trend Graphs" paper
+        # by Zhenhua Pan, Hong Bao, Feng Pan, Cheng Xu
+        # link: http://ijssst.info/Vol-17/No-30/paper19.pdf
+        smooth_acc = ((linear_vel * linear_vel) - (filt_current_vel * filt_current_vel))
           
-        rospy.loginfo('smooth acceleration: %f', smooth_acc)
-        rospy.loginfo('linear_vel: %f', linear_vel)
+        #rospy.loginfo('smooth acceleration: %f', smooth_acc)
+        #rospy.loginfo('linear_vel: %f', linear_vel)
         #rospy.loginfo('angular_vel: %.3f   linear_vel: %.3f   filt_current_vel: %.3f   vel_error: %.3f  acceleration: %.3f', angular_vel, linear_vel, filt_current_vel, vel_error, acceleration)
         
         if smooth_acc >= 0:
             # converting smooth_acc values to throttle acceptable values
-            throttle = smooth_acc * (max_throttle_percent - (linear_vel * 0.018)) / ((11.1111 * 11.1111)/(2*30)) + (linear_vel * 0.018)
+            throttle = smooth_acc * (max_throttle_percent - (linear_vel * 0.018)) / (self.max_vel * self.max_vel) + (linear_vel * 0.018)
         else:
             throttle = 0
         
